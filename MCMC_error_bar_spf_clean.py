736b5fd51a860f6609feace312f8b26f3416cff5
Johan Mazoyer <johan.mazoyer@jpl.nasa.gov>
2019-09-03 17:07:37 -0700

    created a clean spf plot code

---
+++ MCMC_error_bar_spf_clean.py
@@ -0,0 +1,358 @@
+import sys
+import numpy as np
+import astropy.io.fits as fits
+from scipy.ndimage import rotate
+import matplotlib.pyplot as plot
+import yaml
+import csv
+from scipy import optimize
+from emcee import backends
+
+import yaml
+
+def hg_2g(scatt_angles, g1, g2, alpha, Norm):
+
+        scattered_angles_rad=np.radians(scatt_angles)
+        cos_phi = np.cos(scattered_angles_rad)
+
+        g1_2 = g1*g1 #First HG g squared
+        g2_2 = g2*g2 #Second HG g squared
+        #Constant for HG function
+        k=1./(4*np.pi)
+
+        #Henyey Greenstein function
+        hg1=k*alpha*(1. - g1_2)/(1. + g1_2 - (2*g1*cos_phi))**1.5
+        hg2=k*(1-alpha)*(1. - g2_2)/(1. + g2_2 - (2*g2*cos_phi))**1.5
+        hg = hg1+hg2
+        hg_norm = hg/hg[np.where(scatt_angles == 90)] *Norm
+        return hg_norm
+
+def hg_3g(scatt_angles, g1, g2, g3, alpha1, alpha2, Norm):
+
+        scattered_angles_rad=np.radians(scatt_angles)
+        cos_phi = np.cos(scattered_angles_rad)
+
+        g1_2 = g1*g1 #First HG g squared
+        g2_2 = g2*g2 #Second HG g squared
+        g3_2 = g3*g3 #Third HG g squared
+
+        #Constant for HG function
+        k=1./(4*np.pi)
+
+        #Henyey Greenstein function
+        hg1=k*(1. - g1_2)/(1. + g1_2 - (2*g1*cos_phi))**1.5
+        hg2=k*(1. - g2_2)/(1. + g2_2 - (2*g2*cos_phi))**1.5
+        hg3=k*(1. - g3_2)/(1. + g3_2 - (2*g3*cos_phi))**1.5
+        hg = alpha1*hg1 + alpha2 * hg2 + (1 - alpha1 -alpha2) * hg3
+
+        hg_norm = hg/hg[np.where(scatt_angles == 90)] *Norm
+
+        return hg_norm
+
+def log_hg_2g(scatt_angles, g1, g2, alpha, Norm):
+
+        scattered_angles_rad=np.radians(scatt_angles)
+        cos_phi = np.cos(scattered_angles_rad)
+
+        g1_2 = g1*g1 #First HG g squared
+        g2_2 = g2*g2 #Second HG g squared
+        #Constant for HG function
+        k=1./(4*np.pi)
+
+        #Henyey Greenstein function
+        hg1=k*alpha*(1. - g1_2)/(1. + g1_2 - (2*g1*cos_phi))**1.5
+        hg2=k*(1-alpha)*(1. - g2_2)/(1. + g2_2 - (2*g2*cos_phi))**1.5
+        hg = hg1+hg2
+        hg_norm = hg/hg[np.where(scatt_angles == 90)] *Norm
+        return np.log(hg_norm)
+
+def log_hg_3g(scatt_angles,g1, g2, g3, alpha1, alpha2, Norm):
+
+        scattered_angles_rad=np.radians(scatt_angles)
+        cos_phi = np.cos(scattered_angles_rad)
+
+        g1_2 = g1*g1 #First HG g squared
+        g2_2 = g2*g2 #Second HG g squared
+        g3_2 = g3*g3 #Third HG g squared
+
+        #Constant for HG function
+        k=1./(4*np.pi)
+
+        #Henyey Greenstein function
+        hg1=k*(1. - g1_2)/(1. + g1_2 - (2*g1*cos_phi))**1.5
+        hg2=k*(1. - g2_2)/(1. + g2_2 - (2*g2*cos_phi))**1.5
+        hg3=k*(1. - g3_2)/(1. + g3_2 - (2*g3*cos_phi))**1.5
+        hg = alpha1*hg1 + alpha2 * hg2 + (1 - alpha1 -alpha2) * hg3
+
+        hg_norm = hg/hg[np.where(scatt_angles == 90)] *Norm
+        return np.log(hg_norm)
+
+
+
+def measure_spf_errors(yaml_file,Number_rand_mcmc, Norm_90_inplot = 1. ):
+
+
+        with open('initialization_files/' + yaml_file+'.yaml', 'r') as yaml_file:
+                params_mcmc_yaml = yaml.load(yaml_file, Loader=yaml.FullLoader)
+
+        dico_return = dict()
+        burnin = params_mcmc_yaml['BURNIN']
+        thin = params_mcmc_yaml['THIN']
+        n_dim_mcmc = params_mcmc_yaml['N_DIM_MCMC']
+
+
+        band_dir = params_mcmc_yaml['BAND_DIR']
+
+        datadir = basedir + band_dir
+
+        mcmcresultdir = datadir + 'results_MCMC/'
+
+        file_prefix = params_mcmc_yaml['FILE_PREFIX']
+        name_h5 = file_prefix+"_backend_file_mcmc"
+
+
+        chain_name = mcmcresultdir + name_h5 + ".h5"
+        bestmodel_name = mcmcresultdir + name_h5 + "_BestModel.fits"
+
+        reader = backends.HDFBackend(chain_name)
+        chain_flat = reader.get_chain(discard=burnin, flat =True, thin=thin)
+
+
+
+        if n_dim_mcmc == 11:
+                g1_chain = chain_flat[:,3]
+                g2_chain = chain_flat[:,4]
+                alph1_chain = chain_flat[:,5]
+                norm_chain = np.exp(chain_flat[:,10])
+
+        if n_dim_mcmc == 13:
+                g1_chain = chain_flat[:,3]
+                g2_chain = chain_flat[:,4]
+                g3_chain = chain_flat[:,5]
+                alph1_chain = chain_flat[:,6]
+                alph2_chain = chain_flat[:,7]
+                norm_chain = np.exp(chain_flat[:,12])
+
+
+        bestmodel_g1 = np.percentile(g1_chain, 50)
+        bestmodel_g2 = np.percentile(g2_chain, 50)
+        bestmodel_alpha1 = np.percentile(alph1_chain, 50)
+        bestmodel_Norm = np.percentile(norm_chain, 50)
+
+        if n_dim_mcmc == 11:
+                best_hg_mcmc = hg_2g(scattered_angles, bestmodel_g1,bestmodel_g2, bestmodel_alpha1, Norm_90_inplot)
+
+        if n_dim_mcmc == 13:
+                bestmodel_g3 = np.percentile(g3_chain, [50])
+                bestmodel_alpha2 = np.percentile(alph2_chain, [50])
+                best_hg_mcmc = hg_3g(scattered_angles, bestmodel_g1,bestmodel_g2, bestmodel_g3, bestmodel_alpha1, bestmodel_alpha2, Norm_90_inplot)
+
+        dico_return['best_spf'] = best_hg_mcmc
+
+
+        random_param_number = np.random.randint(1,len(g1_chain)-1, Number_rand_mcmc )
+
+        g1_rand = g1_chain[random_param_number]
+        g2_rand = g2_chain[random_param_number]
+        alph1_rand = alph1_chain[random_param_number]
+        norm_rand = norm_chain[random_param_number]
+
+        if n_dim_mcmc == 13:
+                g3_rand = g3_chain[random_param_number]
+                alph2_rand = alph2_chain[random_param_number]
+
+
+        hg_mcmc_rand = np.zeros((len(best_hg_mcmc), len(random_param_number)))
+
+
+        for i, g1_here in enumerate(g1_rand):
+
+                g2_here = g2_rand[i]
+                alph1_here = alph1_rand[i]
+                norm_here = norm_rand[i]
+                if n_dim_mcmc == 11:
+                        hg_mcmc_rand[:,i] = hg_2g(scattered_angles, g1_here,g2_here,alph1_here,norm_here/bestmodel_Norm*Norm_90_inplot)
+                if n_dim_mcmc == 13:
+                        g3_here = g3_rand[i]
+                        alph2_here = alph2_rand[i]
+                        hg_mcmc_rand[:,i] = hg_3g(scattered_angles, g1_here,g2_here,g3_here, alph1_here,alph2_here,norm_here/bestmodel_Norm*Norm_90_inplot)
+
+
+        errorbar_sup = scattered_angles*0.
+        errorbar_inf = scattered_angles*0.
+        errorbar= scattered_angles*0.
+
+        for i, scattered_anglei in enumerate(scattered_angles):
+                errorbar_sup[i] = np.max(hg_mcmc_rand[i,:]) #- best_hg_mcmc[i]
+                errorbar_inf[i] =   np.min(hg_mcmc_rand[i,:]) #- best_hg_mcmc[i]
+                errorbar[i] =  (np.max(hg_mcmc_rand[i,:]) - np.min(hg_mcmc_rand[i,:]))/2.
+
+
+        dico_return['errorbar_sup'] = errorbar_sup
+        dico_return['errorbar_inf'] = errorbar_inf
+        dico_return['errorbar'] = errorbar
+
+        return dico_return
+
+
+
+
+basedir='/Users/jmazoyer/Dropbox/ExchangeFolder/data_python/Aurora/'
+
+min_scat =  13.3
+max_scat =  166.7
+
+scattered_angles = np.arange(np.round(max_scat - min_scat)) +np.round(np.min(min_scat))
+
+
+# ##########################################################################################
+# ### SPHERE H2 exctracted by Milli et al. 2017
+# ##########################################################################################
+
+# sphere spf extracted by Milli et al. 2017
+angles_sphere_extractJulien = np.zeros(49)
+spf_shpere_extractJulien = np.zeros(49)
+errors_sphere_extractJulien = np.zeros(49)
+
+i = 0
+with open(basedir + 'SPHERE_Hdata/' + 'SPHERE_extraction_Milli.csv', 'rt') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        angles_sphere_extractJulien[i] = float(row[0])
+        spf_shpere_extractJulien[i] = float(row[1])
+        errors_sphere_extractJulien[i] = float(row[2])
+        i+=1
+
+
+
+# fit a 3g function to sphere spf extracted by Milli et al. 2017
+log_spf_sphere = np.log(spf_shpere_extractJulien)
+log_errors_sphere = np.log(spf_shpere_extractJulien + errors_sphere_extractJulien) -  np.log(spf_shpere_extractJulien)
+
+initial_guess = [ 0.99997858 , 0.05095741 , 0.04765255 , 0.99946266 , -0.03396517 , 0.94720999]
+params = initial_guess
+a = optimize.curve_fit(log_hg_3g, angles_sphere_extractJulien, log_spf_sphere, initial_guess, log_errors_sphere, maxfev = 10000, bounds=([-1, -1., -1., -1., -1., -1.], [1, 1., 1., 1., 1., 4.]))
+params_3g_fir_from_extraction = a[0]
+hg3g_fitted_Milliextraction = hg_3g(scattered_angles, params_3g_fir_from_extraction[0],params_3g_fir_from_extraction[1],params_3g_fir_from_extraction[2],params_3g_fir_from_extraction[3],params_3g_fir_from_extraction[4], params_3g_fir_from_extraction[5])
+
+
+
+# ##############################
+# # GPI K1 extracted by Pauline from Pol
+# ##############################
+
+dir_exctract_Pauline = basedir + '150403_K1_Spec/Pauline_SPF_from_Pol/'
+
+data_exctract_Pauline = dir_exctract_Pauline + 'Pauline_SPF_ktot_NE_errs.txt'
+text_array = np.loadtxt(data_exctract_Pauline)
+angles_GPIPolK1_extractPauline =  text_array[:, 0]
+spf_GPIPolK1_extractPaulineNE = text_array[:, 1]
+errors_GPIPolK1_extractPaulineNE =  text_array[:, 2]
+
+
+data_exctract_Pauline = dir_exctract_Pauline + 'Pauline_SPF_ktot_SW_errs.txt'
+
+text_array = np.loadtxt(data_exctract_Pauline)
+# angles_GPIPolK1_extractPaulineSW =  text_array[:, 0] ## identical to NE
+spf_GPIPolK1_extractPaulineSW = text_array[:, 1]
+errors_GPIPolK1_extractPaulineSW =  text_array[:, 2]
+
+
+spf_GPIPolK1_extractPauline = 0.5*(spf_GPIPolK1_extractPaulineSW + spf_GPIPolK1_extractPaulineNE)
+
+error_GPIPolK1_extractPauline = np.sqrt(errors_GPIPolK1_extractPaulineSW**2 + errors_GPIPolK1_extractPaulineNE**2)
+
+error_GPIPolK1_extractPauline = error_GPIPolK1_extractPauline/spf_GPIPolK1_extractPauline[np.where(angles_GPIPolK1_extractPauline == 90)]
+spf_GPIPolK1_extractPauline = spf_GPIPolK1_extractPauline/spf_GPIPolK1_extractPauline[np.where(angles_GPIPolK1_extractPauline == 90)]
+
+index = (np.arange((error_GPIPolK1_extractPauline.shape[0])/2) )*2 +1
+
+angles_GPIPolK1_extractPauline = np.delete(angles_GPIPolK1_extractPauline, index)
+spf_GPIPolK1_extractPauline = np.delete(spf_GPIPolK1_extractPauline, index)
+error_GPIPolK1_extractPauline = np.delete(error_GPIPolK1_extractPauline, index)
+
+
+
+
+spf_sphere_h = measure_spf_errors('SPHERE_Hband_MCMC',500)
+spf_sphere_h_3g = measure_spf_errors('SPHERE_Hband_3g_MCMC',500)
+spf_gpi_j = measure_spf_errors('GPI_Jband_MCMC',500)
+spf_gpi_h = measure_spf_errors('GPI_Hband_MCMC',500, Norm_90_inplot= 0.93)
+spf_gpi_k1 = measure_spf_errors('GPI_K1band_MCMC',500)
+spf_gpi_k2 = measure_spf_errors('GPI_K2band_MCMC',500)
+
+
+
+plot.figure()
+
+color0 = 'black'
+color1 = '#3B73FF'
+color2 = '#ED0052'
+color3 = '#00AF64'
+color4 = '#FFCF0B'
+
+
+# name_pdf = 'compare_GPI_color.pdf'
+
+# name_pdf = 'compare_SPHERE_GPI_H.pdf'
+
+
+# plot.plot(scattered_angles, spf_sphere_h['best_spf'],linewidth = 2, color = color0, label="SPHERE H2 (extraction MCMC, this work)")
+# plot.fill_between(scattered_angles,spf_sphere_h['errorbar_sup'], spf_sphere_h['errorbar_inf'], facecolor=color0, alpha=0.2)
+
+# plot.plot(scattered_angles, spf_gpi_h['best_spf'],linewidth = 2, color = color1, label="GPI H")
+# plot.fill_between(scattered_angles,spf_gpi_h['errorbar_sup'], spf_gpi_h['errorbar_inf'], facecolor=color1, alpha=0.2)
+
+# plot.plot(scattered_angles, spf_gpi_j['best_spf'],linewidth = 2, color = color4, label="GPI J")
+# plot.fill_between(scattered_angles,spf_gpi_j['errorbar_sup'], spf_gpi_j['errorbar_inf'], facecolor=color4, alpha=0.2)
+
+# plot.plot(scattered_angles, spf_gpi_k1['best_spf'],linewidth = 2, color = color2, label="GPI K1")
+# plot.fill_between(scattered_angles,spf_gpi_k1['errorbar_sup'], spf_gpi_k1['errorbar_inf'], facecolor=color2, alpha=0.2)
+
+# plot.plot(scattered_angles, spf_gpi_k2['best_spf'],linewidth = 2, color = color3, label="GPI k2")
+# plot.fill_between(scattered_angles,spf_gpi_k2['errorbar_sup'], spf_gpi_k2['errorbar_inf'], facecolor=color3, alpha=0.2)
+
+
+# plot.errorbar(angles_sphere_extractJulien, 1.38*spf_shpere_extractJulien, yerr= errors_sphere_extractJulien, fmt = 'o', label='SPHERE H2 (extraction Milli et al. 2017)', ms =3, capthick = 1, capsize=2, elinewidth=1, markeredgewidth=1, color = 'grey')
+
+# plot.errorbar(angles_GPIPolK1_extractPauline, spf_GPIPolK1_extractPauline, yerr= error_GPIPolK1_extractPauline, fmt = 'x', label='GPI Pol K1 (extraction P. Arriaga)', ms =3, capthick = 1, capsize=2, elinewidth=1, markeredgewidth=1, color = color3)
+
+# plot.legend()
+
+
+### 3g plot
+name_pdf = 'compare_3g_SPF.pdf'
+
+plot.plot(scattered_angles, spf_sphere_h['best_spf'],linewidth = 2, color = color0, label="2 HG SPF extraction MCMC (this work)")
+plot.fill_between(scattered_angles,spf_sphere_h['errorbar_sup'], spf_sphere_h['errorbar_inf'], facecolor=color0, alpha=0.2)
+
+plot.plot(scattered_angles, spf_sphere_h_3g['best_spf'],linewidth = 2, color = color1, label="3 HG SPF extraction MCMC (this work)")
+plot.fill_between(scattered_angles,spf_sphere_h_3g['errorbar_sup'], spf_sphere_h_3g['errorbar_inf'], facecolor=color1, alpha=0.2)
+
+plot.errorbar(angles_sphere_extractJulien, 1.38*spf_shpere_extractJulien, yerr= errors_sphere_extractJulien, fmt = 'o', label='SPHERE H2 (extraction Milli et al. 2017)', ms =3, capthick = 1, capsize=2, elinewidth=1, markeredgewidth=1, color = 'grey')
+plot.plot(scattered_angles, 1.38*hg3g_fitted_Milliextraction,linewidth = 2, color = color3, label="3 HG SPF fit to the SPF extracted in Milli et al. 2017")
+
+
+handles, labels = plot.gca().get_legend_handles_labels()
+order = [3,2,1,0]
+plot.legend([handles[idx] for idx in order],[labels[idx] for idx in order])
+
+
+
+plot.yscale('log')
+
+plot.ylim(bottom=0.3, top=30)
+plot.xlim(left=0, right=180)
+plot.xlabel('Scattering angles')
+plot.ylabel('Normalized total intensity')
+
+plot.tight_layout()
+
+folder_save_pdf = basedir + 'Spf_plots_produced/'
+plot.savefig(folder_save_pdf +name_pdf )
+
+plot.close()
+
+
+
+
