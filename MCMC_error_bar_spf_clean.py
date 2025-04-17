2b9373f6b470a49be21628796d4b87ec5b4dffc7
Johan Mazoyer <johan.mazoyer@obspm.fr>
2025-04-17 11:31:06 +0200

    up

--- MCMC_error_bar_spf_clean.py
+++ MCMC_error_bar_spf_clean.py
@@ -40,7 +40,6 @@ def measure_spf_errors(yaml_file_str, Number_rand_mcmc, Norm_90_inplot=1., save 
     dico_return = dict()
 
     nwalkers = params_mcmc_yaml['NWALKERS']
-    n_dim_mcmc = params_mcmc_yaml['N_DIM_MCMC']
 
     datadir = os.path.join(basedir, params_mcmc_yaml['BAND_DIR'])
 
@@ -228,10 +227,10 @@ def measure_spf_errors(yaml_file_str, Number_rand_mcmc, Norm_90_inplot=1., save 
     return dico_return
 
 if __name__ == '__main__':
-    basedir = os.environ["EXCHANGE_PATH"]  # the base directory where is
+    # basedir = os.environ["EXCHANGE_PATH"]  # the base directory where is
     # your data (using OS environnement variable allow to use same code on
     # different computer without changing this).
-    # basedir = '/Users/jmazoyer/Dropbox/ExchangeFolder/data_python/Aurora/'
+    basedir = '/Users/jmazoyer/Dropbox/ExchangeFolder/data_python/tycho/'
 
     min_scat = 13.3
     max_scat = 166.7
@@ -260,94 +259,96 @@ if __name__ == '__main__':
     spf_shpere_extractJulien = np.zeros(49)
     errors_sphere_extractJulien = np.zeros(49)
 
+
+
     i = 0
-    with open(os.path.join(basedir, 'SPHERE_Hdata', 'SPHERE_extraction_Milli.csv'),
-            'rt') as f:
-        readercsv = csv.reader(f)
-        for row in readercsv:
-            angles_sphere_extractJulien[i] = float(row[0])
-            spf_shpere_extractJulien[i] = float(row[1])
-            errors_sphere_extractJulien[i] = float(row[2])
-            i += 1
+    # with open(os.path.join(basedir, 'SPHERE_Hdata', 'SPHERE_extraction_Milli.csv'),
+    #         'rt') as f:
+    #     readercsv = csv.reader(f)
+    #     for row in readercsv:
+    #         angles_sphere_extractJulien[i] = float(row[0])
+    #         spf_shpere_extractJulien[i] = float(row[1])
+    #         errors_sphere_extractJulien[i] = float(row[2])
+    #         i += 1
 
     # fit a 3g function to sphere spf extracted by Milli et al. 2017
-    log_spf_sphere = np.log(spf_shpere_extractJulien)
-    log_errors_sphere = np.log(spf_shpere_extractJulien +
-                            errors_sphere_extractJulien) - np.log(
-                                spf_shpere_extractJulien)
+    # log_spf_sphere = np.log(spf_shpere_extractJulien)
+    # log_errors_sphere = np.log(spf_shpere_extractJulien +
+    #                         errors_sphere_extractJulien) - np.log(
+    #                             spf_shpere_extractJulien)
 
-    initial_guess = [
-        0.99997858, 0.05095741, 0.04765255, 0.99946266, -0.03396517, 0.94720999
-    ]
-    params = initial_guess
-    a = optimize.curve_fit(log_hg_3g,
-                        angles_sphere_extractJulien,
-                        log_spf_sphere,
-                        initial_guess,
-                        log_errors_sphere,
-                        maxfev=10000,
-                        bounds=([-1, -1., -1., -1., -1.,
-                                    -1.], [1, 1., 1., 1., 1., 4.]))
-    params_3g_fir_from_extraction = a[0]
-    hg3g_fitted_Milliextraction = hg_3g(
-        scattered_angles, params_3g_fir_from_extraction[0],
-        params_3g_fir_from_extraction[1], params_3g_fir_from_extraction[2],
-        params_3g_fir_from_extraction[3], params_3g_fir_from_extraction[4],
-        params_3g_fir_from_extraction[5])
+    # initial_guess = [
+    #     0.99997858, 0.05095741, 0.04765255, 0.99946266, -0.03396517, 0.94720999
+    # ]
+    # params = initial_guess
+    # a = optimize.curve_fit(log_hg_3g,
+    #                     angles_sphere_extractJulien,
+    #                     log_spf_sphere,
+    #                     initial_guess,
+    #                     log_errors_sphere,
+    #                     maxfev=10000,
+    #                     bounds=([-1, -1., -1., -1., -1.,
+    #                                 -1.], [1, 1., 1., 1., 1., 4.]))
+    # params_3g_fir_from_extraction = a[0]
+    # hg3g_fitted_Milliextraction = hg_3g(
+    #     scattered_angles, params_3g_fir_from_extraction[0],
+    #     params_3g_fir_from_extraction[1], params_3g_fir_from_extraction[2],
+    #     params_3g_fir_from_extraction[3], params_3g_fir_from_extraction[4],
+    #     params_3g_fir_from_extraction[5])
 
     # ##############################
     # # GPI K1 extracted by Pauline from Pol
     # ##############################
 
-    dir_exctract_Pauline = os.path.join(basedir, '150403_K1_Spec',
-                                        'Pauline_SPF_from_Pol')
+    # dir_exctract_Pauline = os.path.join(basedir, '150403_K1_Spec',
+    #                                     'Pauline_SPF_from_Pol')
 
-    data_exctract_Pauline = os.path.join(dir_exctract_Pauline,
-                                        'Pauline_SPF_ktot_NE_errs.txt')
-    text_array = np.loadtxt(data_exctract_Pauline)
-    angles_GPIPolK1_extractPauline = text_array[:, 0]
-    spf_GPIPolK1_extractPaulineNE = text_array[:, 1]
-    errors_GPIPolK1_extractPaulineNE = text_array[:, 2]
+    # data_exctract_Pauline = os.path.join(dir_exctract_Pauline,
+    #                                     'Pauline_SPF_ktot_NE_errs.txt')
+    # text_array = np.loadtxt(data_exctract_Pauline)
+    # angles_GPIPolK1_extractPauline = text_array[:, 0]
+    # spf_GPIPolK1_extractPaulineNE = text_array[:, 1]
+    # errors_GPIPolK1_extractPaulineNE = text_array[:, 2]
 
-    data_exctract_Pauline = os.path.join(dir_exctract_Pauline,
-                                        'Pauline_SPF_ktot_SW_errs.txt')
+    # data_exctract_Pauline = os.path.join(dir_exctract_Pauline,
+    #                                     'Pauline_SPF_ktot_SW_errs.txt')
 
-    text_array = np.loadtxt(data_exctract_Pauline)
-    # angles_GPIPolK1_extractPaulineSW =  text_array[:, 0] ## identical to NE
-    spf_GPIPolK1_extractPaulineSW = text_array[:, 1]
-    errors_GPIPolK1_extractPaulineSW = text_array[:, 2]
+    # text_array = np.loadtxt(data_exctract_Pauline)
+    # # angles_GPIPolK1_extractPaulineSW =  text_array[:, 0] ## identical to NE
+    # spf_GPIPolK1_extractPaulineSW = text_array[:, 1]
+    # errors_GPIPolK1_extractPaulineSW = text_array[:, 2]
 
-    spf_GPIPolK1_extractPauline = 0.5 * (spf_GPIPolK1_extractPaulineSW +
-                                        spf_GPIPolK1_extractPaulineNE)
+    # spf_GPIPolK1_extractPauline = 0.5 * (spf_GPIPolK1_extractPaulineSW +
+    #                                     spf_GPIPolK1_extractPaulineNE)
 
-    error_GPIPolK1_extractPauline = np.sqrt(errors_GPIPolK1_extractPaulineSW**2 +
-                                            errors_GPIPolK1_extractPaulineNE**2)
+    # error_GPIPolK1_extractPauline = np.sqrt(errors_GPIPolK1_extractPaulineSW**2 +
+    #                                         errors_GPIPolK1_extractPaulineNE**2)
 
-    error_GPIPolK1_extractPauline = error_GPIPolK1_extractPauline / spf_GPIPolK1_extractPauline[
-        np.where(angles_GPIPolK1_extractPauline == 90)]
-    spf_GPIPolK1_extractPauline = spf_GPIPolK1_extractPauline / spf_GPIPolK1_extractPauline[
-        np.where(angles_GPIPolK1_extractPauline == 90)]
+    # error_GPIPolK1_extractPauline = error_GPIPolK1_extractPauline / spf_GPIPolK1_extractPauline[
+    #     np.where(angles_GPIPolK1_extractPauline == 90)]
+    # spf_GPIPolK1_extractPauline = spf_GPIPolK1_extractPauline / spf_GPIPolK1_extractPauline[
+    #     np.where(angles_GPIPolK1_extractPauline == 90)]
 
-    index = (np.arange((error_GPIPolK1_extractPauline.shape[0]) / 2)) * 2 + 1
+    # index = (np.arange((error_GPIPolK1_extractPauline.shape[0]) / 2)) * 2 + 1
 
-    angles_GPIPolK1_extractPauline = np.delete(angles_GPIPolK1_extractPauline,
-                                            index)
-    spf_GPIPolK1_extractPauline = np.delete(spf_GPIPolK1_extractPauline, index)
-    error_GPIPolK1_extractPauline = np.delete(error_GPIPolK1_extractPauline, index)
+    # angles_GPIPolK1_extractPauline = np.delete(angles_GPIPolK1_extractPauline,
+    #                                         index)
+    # spf_GPIPolK1_extractPauline = np.delete(spf_GPIPolK1_extractPauline, index)
+    # error_GPIPolK1_extractPauline = np.delete(error_GPIPolK1_extractPauline, index)
 
-    spf_sphere_h = measure_spf_errors('SPHERE_Hband_MCMC', nb_random_models)
-    spf_sphere_h_3g = measure_spf_errors('SPHERE_Hband_3g_MCMC', nb_random_models)
-    spf_gpi_not1at90 = measure_spf_errors('GPI_Hband_MCMC',
-                                        nb_random_models,
-                                        Norm_90_inplot=0.93)
-    spf_gpi_h_1at90 = measure_spf_errors('GPI_Hband_MCMC', nb_random_models)
-    spf_gpi_j = measure_spf_errors('GPI_Jband_MCMC', nb_random_models)
-    spf_gpi_k1 = measure_spf_errors('GPI_K1band_MCMC', nb_random_models)
-    spf_gpi_k2 = measure_spf_errors('GPI_K2band_MCMC', nb_random_models)
+    spf_sphere_h = measure_spf_errors('SPHERE_Hband_MCMC_ADI', nb_random_models, Norm_90_inplot=0.08)
+    # spf_sphere_h_3g = measure_spf_errors('SPHERE_Hband_3g_MCMC', nb_random_models)
+    # spf_gpi_not1at90 = measure_spf_errors('GPI_Hband_MCMC',
+    #                                     nb_random_models,
+    #                                     Norm_90_inplot=0.93)
+    # spf_gpi_h_1at90 = measure_spf_errors('GPI_Hband_MCMC', nb_random_models)
+    # spf_gpi_j = measure_spf_errors('GPI_Jband_MCMC', nb_random_models)
+    # spf_gpi_k1 = measure_spf_errors('GPI_K1band_MCMC', nb_random_models)
+    # spf_gpi_k2 = measure_spf_errors('GPI_K2band_MCMC', nb_random_models)
 
-    spf_gpi_h_fake = measure_spf_errors('GPI_Hband_fake_MCMC', nb_random_models)
+    # spf_gpi_h_fake = measure_spf_errors('GPI_Hband_fake_MCMC', nb_random_models)
 
-    spf_gpi_h_save = measure_spf_errors('GPI_Hband_MCMC', nb_random_models, save = True)
+    # spf_gpi_h_save = measure_spf_errors('GPI_Hband_MCMC', nb_random_models, save = True)
 
     color0 = 'black'
     color1 = '#3B73FF'
@@ -367,35 +368,41 @@ if __name__ == '__main__':
                     facecolor=color0,
                     alpha=0.1)
 
-    plot.fill_between(scattered_angles,
-                    spf_gpi_not1at90['errorbar_sup'],
-                    spf_gpi_not1at90['errorbar_inf'],
-                    facecolor=color1,
-                    alpha=0.1)
+    # plot.fill_between(scattered_angles,
+    #                 spf_gpi_not1at90['errorbar_sup'],
+    #                 spf_gpi_not1at90['errorbar_inf'],
+    #                 facecolor=color1,
+    #                 alpha=0.1)
 
     plot.plot(scattered_angles,
             spf_sphere_h['best_spf'],
             linewidth=2,
-            color=color0,
-            label="SPHERE H2 (extraction MCMC, this work)")
+            label="HR 4796 SPHERE (Chen 2020)")
 
-    plot.plot(scattered_angles,
-            spf_gpi_not1at90['best_spf'],
-            linewidth=2,
-            color=color1,
-            label="GPI H (extraction MCMC)")
+    # scatter_angles_hong = np.arange(np.round(180 - 15)) + np.round(np.min(15))
+    # Hong85_zodiac_spf  = hg_3g(scatter_angles_hong, 0.7, -0.2, -0.81, 0.665,0.330, 0.08)
+    # plot.plot(scatter_angles_hong,
+    #         Hong85_zodiac_spf,
+    #         linewidth=2,
+    #         label="Zodiacal dust (Hong et al.)")
+    
+    # plot.plot(scattered_angles,
+    #         spf_gpi_not1at90['best_spf'],
+    #         linewidth=2,
+    #         color=color1,
+    #         label="GPI H (extraction MCMC)")
 
-    plot.errorbar(angles_sphere_extractJulien,
-                1.38 * spf_shpere_extractJulien,
-                yerr=errors_sphere_extractJulien,
-                fmt='o',
-                label='SPHERE H2 (previous extraction by M17)',
-                ms=3,
-                capthick=1,
-                capsize=2,
-                elinewidth=1,
-                markeredgewidth=1,
-                color='grey')
+    # plot.errorbar(angles_sphere_extractJulien,
+    #             1.38 * spf_shpere_extractJulien,
+    #             yerr=errors_sphere_extractJulien,
+    #             fmt='o',
+    #             label='SPHERE H2 (previous extraction by M17)',
+    #             ms=3,
+    #             capthick=1,
+    #             capsize=2,
+    #             elinewidth=1,
+    #             markeredgewidth=1,
+    #             color='grey')
 
     # plot.errorbar(angles_GPIPolK1_extractPauline,
     #               spf_GPIPolK1_extractPauline,
@@ -412,17 +419,18 @@ if __name__ == '__main__':
     plot.legend()
     plot.yscale('log')
 
-    plot.ylim(bottom=0.3, top=30)
+    plot.ylim(bottom=0.018, top=200)
     plot.xlim(left=0, right=180)
     plot.xlabel('Scattering angles')
     plot.ylabel('Normalized total intensity')
 
     plot.tight_layout()
 
-    plot.savefig(os.path.join(folder_save_pdf, name_pdf))
+    plot.savefig(os.path.join(folder_save_pdf, name_pdf), transparent = True)
 
     plot.close()
 
+    asd
     ####################################################################################
     ## plot all colors
     ####################################################################################
